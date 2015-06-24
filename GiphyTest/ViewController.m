1c989dc06b9c314576d1f226d44fc0183ed9b817
Alexander <maxatma@Alexanders-MacBook-Pro.local>
2015-06-24 10:17:08 +0300

    Initial Commit

---
+++ GiphyTest/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  GiphyTest
+//
+//  Created by Alexander on 6/24/15.
+//  Copyright (c) 2015 Alexander. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
